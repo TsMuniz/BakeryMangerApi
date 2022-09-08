27c8f93111e9948ddef036f7d83f540cf78fd127
thiago <ts_muniz@outlook.com>
2022-09-08 13:03:40 -0300

    feat: adiciona tradução a lib admin js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,25 @@
+import 'dotenv/config'
 import AdminJS from 'adminjs'
 import AdminJSExpress from '@adminjs/express'
+import AdminJSSequelize from '@adminjs/sequelize'
 import express from 'express'
+import userResources from './resources/UserResources'
+import projectResources from './resources/ProjectResource'
+import locales from './resources/locales'
+import './database/models'
 
+AdminJS.registerAdapter(AdminJSSequelize)
 const app = express()
 
 const adminJS = new AdminJS({
     databases: [],
     rootPath: '/admin',
-    resources: [],
+    resources: [userResources, projectResources],
+    ...locales
 })
 
 const router = AdminJSExpress.buildRouter(adminJS)
 
 app.use(adminJS.options.rootPath, router)
 
-app.listen(3000, () => console.log(`rodando na porta 5000`))
+app.listen(process.env.PORT, () => console.log(`rodando na porta ${process.env.PORT}`))
